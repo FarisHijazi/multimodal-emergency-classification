4d508ad58bae3f1a1cfef4a51b16912327b295b2
FarisHijazi <theefaris@gmail.com>
2023-07-23 11:25:06 +0300

    updatingi code to work on llama_index

--- demo.py
+++ demo.py
@@ -3,8 +3,8 @@ import os
 from datetime import datetime
 import logging
 import sys
-from gpt_index import SimpleDirectoryReader
-import gpt_index.readers.file.base
+from llama_index import SimpleDirectoryReader
+import llama_index.readers.file.base
 import glob
 import numpy as np
 import soundfile as sf
@@ -13,24 +13,28 @@ import openai
 import json
 import cv2
 
+from llama_index import download_loader
+
+ImageCaptionReader = download_loader('ImageCaptionReader')
+
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-print('done processing import')
+# print('done processing import')
 with open('config.json', encoding='utf8') as f:
     config = json.load(f)
 
 def process_inputs(text: str, image: np.ndarray, video: str, audio: tuple, ):
     output = ""
-    # print('video', type(video), video)
-    # print('text', type(text), text)
-    # print('audio', type(audio), audio)
-    # print('image', type(image), image)
+    # # print('video', type(video), video)
+    # # print('text', type(text), text)
+    # # print('audio', type(audio), audio)
+    # # print('image', type(image), image)
     if not text and image is not None and not video and audio is not None:
         return "Please upload at least one of the following: text, image, video, audio."
-    
+
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     # Create a folder named 'media_files' if it doesn't exist
     os.makedirs(f"media_files/{timestamp}", exist_ok=True)
@@ -55,7 +59,7 @@ def process_inputs(text: str, image: np.ndarray, video: str, audio: tuple, ):
         with open(text_path, "w", encoding='utf8') as f:
             f.write(text)
         output += "Text processed and saved: " + text + "\n"
-        print("Text processed and saved: " + text + "")
+        # print("Text processed and saved: " + text + "")
         # gr.Interface.update("Text processed and saved: " + "")
 
     if audio is not None:
@@ -82,78 +86,78 @@ def process_inputs(text: str, image: np.ndarray, video: str, audio: tuple, ):
     text_files = glob.glob(f'{root}/*.txt')
     text_content = ''
     if text_files:
-        print('processing text_files ...')
+        # print('processing text_files ...')
         text_content = SimpleDirectoryReader(
             input_files=text_files,
             file_extractor={
-                ".jpg": gpt_index.readers.file.base.ImageCaptionParser(),
-                ".png": gpt_index.readers.file.base.ImageCaptionParser(),
-                ".jpeg": gpt_index.readers.file.base.ImageParser(),
-                ".wav": gpt_index.readers.file.base.VideoAudioParser(),
-                ".mp4": gpt_index.readers.file.base.VideoAudioParser(),
+                ".jpg": ImageCaptionReader(),
+                ".png": ImageCaptionReader(),
+                ".jpeg": ImageCaptionReader(),
+                ".wav": llama_index.readers.file.video_audio_reader,
+                ".mp4": llama_index.readers.file.video_audio_reader,
             }
         ).load_data()
         texts = [x.text for x in text_content]
         text = '\n\n'.join(texts)
         text_content = text.replace('"', "'").replace('\n', '.  ')
-        print('done processing text_files')
+        # print('done processing text_files')
 
     image_files = glob.glob(f'{root}/*.png') + glob.glob(f'{root}/*.jpg')
     image_content = ''
     if image_files:
-        print('processing image_files ...')
+        # print('processing image_files ...')
         image_content = SimpleDirectoryReader(
             input_files=image_files,
             file_extractor={
-                ".jpg": gpt_index.readers.file.base.ImageCaptionParser(),
-                ".png": gpt_index.readers.file.base.ImageCaptionParser(),
-                ".jpeg": gpt_index.readers.file.base.ImageParser(),
-                ".wav": gpt_index.readers.file.base.VideoAudioParser(),
-                ".mp4": gpt_index.readers.file.base.VideoAudioParser(),
+                ".jpg": ImageCaptionReader(),
+                ".png": ImageCaptionReader(),
+                ".jpeg": ImageCaptionReader(),
+                ".wav": llama_index.readers.file.video_audio_reader,
+                ".mp4": llama_index.readers.file.video_audio_reader,
             }
         ).load_data()
         texts = [x.text for x in image_content]
         text = '\n\n'.join(texts)
         image_content = text.replace('"', "'").replace('\n', '.  ')
-        print('done processing image_files')
+        # print('done processing image_files')
 
     audio_files = glob.glob(f'{root}/*.mp3')
     audio_content = ''
     if audio_files:
-        print('processing audio_files ...')
+        # print('processing audio_files ...')
         audio_content = SimpleDirectoryReader(
             input_files=audio_files,
             file_extractor={
-                ".jpg": gpt_index.readers.file.base.ImageCaptionParser(),
-                ".png": gpt_index.readers.file.base.ImageCaptionParser(),
-                ".jpeg": gpt_index.readers.file.base.ImageParser(),
-                ".mp3": gpt_index.readers.file.base.VideoAudioParser(),
-                ".mp4": gpt_index.readers.file.base.VideoAudioParser(),
+                ".jpg": ImageCaptionReader(),
+                ".png": ImageCaptionReader(),
+                ".jpeg": ImageCaptionReader(),
+                ".mp3": llama_index.readers.file.video_audio_reader,
+                ".mp4": llama_index.readers.file.video_audio_reader,
             }
         ).load_data()
         texts = [x.text for x in audio_content]
         text = '\n\n'.join(texts)
         audio_content = text.replace('"', "'").replace('\n', '.  ')
-        print('done processing audio_files')
+        # print('done processing audio_files')
 
     video_files = glob.glob(f'{root}/*.mp4')
     video_content = ''
     if video_files:
-        print('processing video_files ...')
+        # print('processing video_files ...')
         video_content = SimpleDirectoryReader(
             input_files=video_files,
             file_extractor={
-                ".jpg": gpt_index.readers.file.base.ImageCaptionParser(),
-                ".png": gpt_index.readers.file.base.ImageCaptionParser(),
-                ".jpeg": gpt_index.readers.file.base.ImageParser(),
-                ".mp3": gpt_index.readers.file.base.VideoAudioParser(),
-                ".mp4": gpt_index.readers.file.base.VideoAudioParser(),
+                ".jpg": ImageCaptionReader(),
+                ".png": ImageCaptionReader(),
+                ".jpeg": ImageCaptionReader(),
+                ".mp3": llama_index.readers.file.video_audio_reader,
+                ".mp4": llama_index.readers.file.video_audio_reader,
             }
         ).load_data()
         texts = [x.text for x in video_content]
         text = '\n\n'.join(texts)
         video_content = text.replace('"', "'").replace('\n', '.  ')
-        print('done processing video_files')
+        # print('done processing video_files')
 
 
     ar2en = {v:k for (k,v) in config["en2ar"].items()}
@@ -165,8 +169,9 @@ The incoming information could be Arabic or English, and you must output only in
 
 The different types of emergencies are only one of {len(emergencies_en)}: {json.dumps(emergencies_en)}
 
-I will give you the information provided by the user bellow, and you should classify from the 7 types of emergencies.
+I will give you the information provided by the user bellow, and you should classify from the {len(emergencies_en)} types of emergencies.
 """
+
 
     prompt = """
 
@@ -188,10 +193,10 @@ Note that "explaination_arabic" must be in Arabic.
 
 For the emergency_class, you must choose one of the following: """ + json.dumps(emergencies_en)
 
-    print('prompt', prompt)
+    # print('prompt', prompt)
 
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": prompt},
@@ -203,7 +208,7 @@ For the emergency_class, you must choose one of the following: """ + json.dumps(
     content = content.replace(",}", "}") # just in case
     # start from first "{" until the first "}"
     content = content[content.find("{") : content.find("}")+1]
-    print('ChatGPT response:', content)
+    # print('ChatGPT response:', content)
     try:
         result = json.loads(content)
     except:
@@ -217,10 +222,9 @@ For the emergency_class, you must choose one of the following: """ + json.dumps(
     
     output = f"""نوع الحالة: {emergency_class_ar}
 
-الشرح: {result['explaination_arabic']}
 
 الجهات المسؤولة:
-- """ + ('\n - '.join(redirects))
+- """ + ('\n - '.join(redirects)) + f"\n\nالشرح: {result['explaination_arabic']}"
 
     return output if output else "No input provided."
 
@@ -233,7 +237,10 @@ output_text = gr.outputs.Textbox(label="Output Text")
 examples = [
     # text_input, image_input, video_input, audio_input
     ["", None,"data/fire_at_gas_station.mp4", None,],
-    ["", "data/small-car-accident.jpg", None, None, None,],
+    ["", "data/small-car-accident.jpg", None, None],
+    ["", "data/electrical-fire.jpg", None, None],
+    ["", "data/major-car-accident.jpg", None, None],
+    ["", "data/gettyimages-50908538-612x612.jpg", None, None],
     ["", None, None, "data/fire_at_gas_station.mp3",],
     ["السلام عليكم، أنا أتصل لأبلغ عن حريق كبير في مبنى سكني بشارع المنصور. يبدو أن النيران اندلعت في الطابق الثالث وتنتشر بسرورة. يرجى إرسال رجال الإطفاء فوراً", None, None, None],
     ["السلام عليكم، أنا أتصل لأبلغ عن حادثة تحرش حدثت لي في مترو الأنفاق بمحطة المرج. كان هناك رجل يلمسني بشكل غير لائق ويحاول مضايقتي. يرجى إرسال دورية أمنية للموقع فوراً", None, None, None],
@@ -244,12 +251,18 @@ iface = gr.Interface(
     fn=process_inputs,
     inputs=[text_input, image_input, video_input, audio_input],
     outputs=output_text,
-    title="<img src='https://i.imgur.com/x1m3VDT.jpg' width='100' height='100'> منصة استجابة",
+    title="<img src='https://i.imgur.com/Qakrqvn.png' width='100' height='100'> منصة استجابة",
     description="تحديد نوع المخاطر والحالات الطارئة تلقائيا باستخدام الذكاء الاصطناعي,\nشبيها بتطبيق 'كلنا امن' بامكانك رفع نص او صور اومقطع او صوت وسيتم تحديد نوع الحالة والجهات المسؤولة عنها",
     examples=examples,
     cache_examples=True,
 )
+# image = gr.Image("logo.png", style=(100, 100))
+# iface.add(image)
 
+# "text-align: right;"
+
+# print('http://127.0.0.1:7860/?__theme=light')
 iface.launch(
-    share=True
+    share=True,
+    favicon_path='logo.png'
 )
